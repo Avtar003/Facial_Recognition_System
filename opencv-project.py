59e62cc964a89e456d7085fc000349241b10b52f
Avtar003 <70094422+Avtar003@users.noreply.github.com>
2022-03-28 10:42:56 +0530

    Update opencv-project.py

--- opencv-project.py
+++ opencv-project.py
@@ -1,82 +1,10 @@
-# import cv2
-#
-# def draw_boundary(img,classifier,scaleFactor,minNeighbour,color,text):
-#     gray_image = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-#     faces = classifier.detectMultipleScale(gray_image,scaleFactor,minNeighbour)
-#     cords = []
-#     for (x,y,w,h) in faces:
-#         cv2.rectangle(img,(x,y),(x+w,y+h),color,2)
-#         cv2.putText(img,text,(x,y-4),cv2.FONT_HERSHEY_SIMPLEX,0.8,color,1,cv2.LINE_AA)
-#         cords = [x,y,w,h]
-#     return cords,img
-#
-#
-# def detect(img,faceCascade):
-#     cords,img = draw_boundary(img,faceCascade,1.1,4,(117, 13, 34),'Face')
-#     return img
-#
-#
-# video_capture = cv2.VideoCapture(0)
-# faceCascade = cv2.CascadeClassifier("D:\yadav\Coding Blocks\opencv\haarcascade_frontalface_alt")
-# # face_cascade = cv2.CascadeClassifier("D:\yadav\Coding Blocks\opencv\haarcascade_frontalface_alt")
-#
-# while True:
-#     rect,img = video_capture.read()
-#     img = detect(img,faceCascade)
-#     cv2.imshow('Face', img)
-#
-#     if cv2.waitKey(1) & 0xFF == ord('s'):
-#         break
-#
-# video_capture.release()
-# cv2.destroyAllWindows()
-
-
-# import cv2
-#
-# faceCascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_alt.xml")
-# video_capture = cv2.VideoCapture(0)
-#
-# while video_capture.isOpened():
-#     _,img = video_capture.read()
-#     gray_frame = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-#     faces = faceCascade.detectMultiScale(img,1.1,5)
-#     for (x,y,w,h) in faces:
-#         cv2.rectangle(img,(x,y),(x+w,y+h),(90, 204, 95),2)
-#     cv2.imshow("Face",img)
-#     if cv2.waitKey(1) & 0xFF == ord('q'):
-#         break
-#
-# video_capture.release()
-# cv2.destroyAllWindows()
-
-# import cv2
-#
-# faceCascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_alt.xml")
-# video_capture = cv2.VideoCapture(0)
-#
-# while video_capture.isOpened():
-#     _,img = video_capture.read()
-#     gray_frame = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-#     faces = faceCascade.detectMultiScale(gray_frame,1.1,5)
-#     for (x,y,w,h) in faces:
-#         cv2.rectangle(gray_frame,(x,y),(x+w,y+h),(68, 212, 219),2)
-#     cv2.imshow("Faces",gray_frame)
-#
-#     if cv2.waitKey(1) & 0xFF == ord('q'):
-#         break
-#
-# video_capture.release()
-# cv2.destroyAllWindows()
-
-
 import cv2
 import numpy as np
 
 faceCascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_alt.xml")
 video_capture = cv2.VideoCapture(0)
 skip = 0
-#face_section = 0
+
 face_data = []
 dataset_path = './data/'
 file_name = input('Enter the name of the person : ')
@@ -101,7 +29,6 @@ while video_capture.isOpened():
 
 
     cv2.imshow('Face',img)
-    #cv2.imshow('Section',face_section)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
